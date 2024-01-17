2811c855a093250757d2b9e3040f10375c187b3e
Yedapeda <yedapeda1563@gmail.com>
2024-01-17 02:39:12 -0800

    [ADDED]:Yash - c program is of primeno

---
+++ prime.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+	int flag=1, n, i;
+	printf("\nEnter Any No.:");
+	scanf("%d", &n);
+	if(n==1)
+	{
+		flag=1;
+		
+	}
+	else
+	{
+		for(i=2; i<=n-1; i++)
+		{
+			if(n%i==0)
+			{
+				flag=0;
+				break;
+			}
+		}
+	}
+	if(flag==1)
+	{
+		printf("\n Prime NO.");
+	}
+	else
+	{
+		printf("\nNon Prime No.");
+	}
+}
